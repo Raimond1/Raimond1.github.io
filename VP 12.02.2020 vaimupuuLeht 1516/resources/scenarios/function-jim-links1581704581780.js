94f651d5960e6fd01c1e3ef7a9a1324611ff4a0f
Ŕaimond <rroosad@ttu.ee>
2020-02-14 20:24:22 +0200

    tegelik esimene versioon

---
+++ VP 12.02.2020 vaimupuuLeht 1516/resources/scenarios/function-jim-links1581704581780.js
@@ -0,0 +1,187 @@
+(function(window, undefined) {
+
+  var jimLinks = {
+    "0a30b18f-f7c8-4152-9255-a1b021a75e27" : {
+      "Menu_item_28" : [
+        "e6ccdc57-a4c2-4ccd-955c-a5f060775156"
+      ],
+      "Menu_item_6" : [
+        "44f27c1f-ef89-4f6f-b49e-3641244aafdf"
+      ],
+      "Menu_item_1" : [
+        "824716ea-4918-46e4-8d2b-d7167ece4d04"
+      ],
+      "Menu_item_9" : [
+        "12a88b94-c87f-4e72-9d54-8d7dbaeb6ea1"
+      ],
+      "Menu_item_17" : [
+        "15ada915-806d-41d3-8059-47fd237cecfd"
+      ],
+      "Menu_item_10" : [
+        "42783552-ba61-43b8-a824-cab98679bbd6"
+      ],
+      "Menu_item_25" : [
+        "4ff8ced3-7b63-459f-8656-3546d2961461"
+      ],
+      "Menu_item_26" : [
+        "2d78788e-7413-4087-bc63-6fc3780ce60a"
+      ],
+      "Menu_item_13" : [
+        "da9e8729-7d6b-4776-af32-d4138c7d194f"
+      ],
+      "Menu_item_14" : [
+        "0a30b18f-f7c8-4152-9255-a1b021a75e27"
+      ],
+      "Menu_item_16" : [
+        "082d29f3-f35e-4de2-bb0a-48627066d08f"
+      ],
+      "Menu_item_19" : [
+        "50cfbfa3-a133-481b-b3bb-781fac5f954f"
+      ],
+      "Menu_item_7" : [
+        "122d08c9-e214-426f-9884-55cb675aafde",
+        "50cfbfa3-a133-481b-b3bb-781fac5f954f"
+      ],
+      "Menu_item_23" : [
+        "cc2a8fa6-422b-4126-bee5-c4e05ac33c09"
+      ],
+      "Menu_item_24" : [
+        "e9b4a9c9-e735-4248-8fa4-d29841cdb0f1"
+      ],
+      "Menu_item_22" : [
+        "2f0d710a-009c-4feb-96e5-066dca16d763"
+      ],
+      "Menu_item_18" : [
+        "6f646e6f-8bbc-4c9d-84cb-1fbb2baa311d"
+      ],
+      "Menu_item_20" : [
+        "a6e2720e-88ce-42f0-9bbd-2363bcf0bf91"
+      ],
+      "Menu_item_21" : [
+        "bd400626-3cb2-40a1-b25c-37235ef5b42e"
+      ]
+    },
+    "eea5d5c9-c915-47fe-aad6-d322b19c45a5" : {
+      "Menu_item_28" : [
+        "e6ccdc57-a4c2-4ccd-955c-a5f060775156"
+      ],
+      "Menu_item_6" : [
+        "44f27c1f-ef89-4f6f-b49e-3641244aafdf"
+      ],
+      "Menu_item_1" : [
+        "824716ea-4918-46e4-8d2b-d7167ece4d04"
+      ],
+      "Menu_item_9" : [
+        "12a88b94-c87f-4e72-9d54-8d7dbaeb6ea1"
+      ],
+      "Menu_item_17" : [
+        "15ada915-806d-41d3-8059-47fd237cecfd"
+      ],
+      "Menu_item_10" : [
+        "42783552-ba61-43b8-a824-cab98679bbd6"
+      ],
+      "Menu_item_25" : [
+        "4ff8ced3-7b63-459f-8656-3546d2961461"
+      ],
+      "Menu_item_26" : [
+        "2d78788e-7413-4087-bc63-6fc3780ce60a"
+      ],
+      "Menu_item_13" : [
+        "da9e8729-7d6b-4776-af32-d4138c7d194f"
+      ],
+      "Menu_item_14" : [
+        "0a30b18f-f7c8-4152-9255-a1b021a75e27"
+      ],
+      "Menu_item_16" : [
+        "082d29f3-f35e-4de2-bb0a-48627066d08f"
+      ],
+      "Menu_item_19" : [
+        "50cfbfa3-a133-481b-b3bb-781fac5f954f"
+      ],
+      "Menu_item_7" : [
+        "122d08c9-e214-426f-9884-55cb675aafde",
+        "50cfbfa3-a133-481b-b3bb-781fac5f954f"
+      ],
+      "Menu_item_23" : [
+        "cc2a8fa6-422b-4126-bee5-c4e05ac33c09"
+      ],
+      "Menu_item_24" : [
+        "e9b4a9c9-e735-4248-8fa4-d29841cdb0f1"
+      ],
+      "Menu_item_22" : [
+        "2f0d710a-009c-4feb-96e5-066dca16d763"
+      ],
+      "Menu_item_18" : [
+        "6f646e6f-8bbc-4c9d-84cb-1fbb2baa311d"
+      ],
+      "Menu_item_20" : [
+        "a6e2720e-88ce-42f0-9bbd-2363bcf0bf91"
+      ],
+      "Menu_item_21" : [
+        "bd400626-3cb2-40a1-b25c-37235ef5b42e"
+      ]
+    },
+    "d12245cc-1680-458d-89dd-4f0d7fb22724" : {
+      "Menu_item_28" : [
+        "e6ccdc57-a4c2-4ccd-955c-a5f060775156"
+      ],
+      "Menu_item_6" : [
+        "44f27c1f-ef89-4f6f-b49e-3641244aafdf"
+      ],
+      "Menu_item_1" : [
+        "824716ea-4918-46e4-8d2b-d7167ece4d04"
+      ],
+      "Menu_item_9" : [
+        "12a88b94-c87f-4e72-9d54-8d7dbaeb6ea1"
+      ],
+      "Menu_item_17" : [
+        "15ada915-806d-41d3-8059-47fd237cecfd"
+      ],
+      "Menu_item_10" : [
+        "42783552-ba61-43b8-a824-cab98679bbd6"
+      ],
+      "Menu_item_25" : [
+        "4ff8ced3-7b63-459f-8656-3546d2961461"
+      ],
+      "Menu_item_26" : [
+        "2d78788e-7413-4087-bc63-6fc3780ce60a"
+      ],
+      "Menu_item_13" : [
+        "da9e8729-7d6b-4776-af32-d4138c7d194f"
+      ],
+      "Menu_item_14" : [
+        "0a30b18f-f7c8-4152-9255-a1b021a75e27"
+      ],
+      "Menu_item_16" : [
+        "082d29f3-f35e-4de2-bb0a-48627066d08f"
+      ],
+      "Menu_item_19" : [
+        "50cfbfa3-a133-481b-b3bb-781fac5f954f"
+      ],
+      "Menu_item_7" : [
+        "122d08c9-e214-426f-9884-55cb675aafde",
+        "50cfbfa3-a133-481b-b3bb-781fac5f954f"
+      ],
+      "Menu_item_23" : [
+        "cc2a8fa6-422b-4126-bee5-c4e05ac33c09"
+      ],
+      "Menu_item_24" : [
+        "e9b4a9c9-e735-4248-8fa4-d29841cdb0f1"
+      ],
+      "Menu_item_22" : [
+        "2f0d710a-009c-4feb-96e5-066dca16d763"
+      ],
+      "Menu_item_18" : [
+        "6f646e6f-8bbc-4c9d-84cb-1fbb2baa311d"
+      ],
+      "Menu_item_20" : [
+        "a6e2720e-88ce-42f0-9bbd-2363bcf0bf91"
+      ],
+      "Menu_item_21" : [
+        "bd400626-3cb2-40a1-b25c-37235ef5b42e"
+      ]
+    }    
+  }
+
+  window.jimLinks = jimLinks;
+})(window);
